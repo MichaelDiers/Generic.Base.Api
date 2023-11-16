8128855b45f00b624d140db1407c9962e3a8777c
MichaelDiers <md@sreid.de>
2023-11-17 00:53:34 +0100

    add unauthorized exception

---
+++ WebApi.NetCore/Generic.Base.Api.Tests/Exceptions/UnauthorizedExceptionTests.cs
@@ -0,0 +1,56 @@
+﻿namespace Generic.Base.Api.Tests.Exceptions
+{
+    using Generic.Base.Api.Exceptions;
+
+    /// <summary>
+    ///     Tests for <see cref="UnauthorizedException" />.
+    /// </summary>
+    public class UnauthorizedExceptionTests
+    {
+        [Fact]
+        public void EmptyCtor()
+        {
+            var message = $"Exception of type '{typeof(UnauthorizedException).FullName}' was thrown.";
+            var exception = new UnauthorizedException();
+
+            Assert.IsAssignableFrom<Exception>(exception);
+            Assert.Equal(
+                message,
+                exception.Message);
+            Assert.Null(exception.InnerException);
+        }
+
+        [Fact]
+        public void MessageCtor()
+        {
+            var message = nameof(this.MessageCtor);
+
+            var exception = new UnauthorizedException(message);
+
+            Assert.IsAssignableFrom<Exception>(exception);
+            Assert.Equal(
+                message,
+                exception.Message);
+            Assert.Null(exception.InnerException);
+        }
+
+        [Fact]
+        public void MessageInnerExceptionCtor()
+        {
+            var message = nameof(this.MessageCtor);
+            var innerException = new Exception();
+
+            var exception = new UnauthorizedException(
+                message,
+                innerException);
+
+            Assert.IsAssignableFrom<Exception>(exception);
+            Assert.Equal(
+                message,
+                exception.Message);
+            Assert.Equal(
+                innerException,
+                exception.InnerException);
+        }
+    }
+}
---
+++ WebApi.NetCore/Generic.Base.Api/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,37 @@
+﻿namespace Generic.Base.Api.Exceptions
+{
+    /// <summary>
+    ///     Exception thrown if an item does not exists.
+    /// </summary>
+    /// <seealso cref="Exception" />
+    public class UnauthorizedException : Exception
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="UnauthorizedException" /> class.
+        /// </summary>
+        public UnauthorizedException()
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="UnauthorizedException" /> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public UnauthorizedException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="UnauthorizedException" /> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="inner">The inner exception.</param>
+        public UnauthorizedException(string message, Exception inner)
+            : base(
+                message,
+                inner)
+        {
+        }
+    }
+}
