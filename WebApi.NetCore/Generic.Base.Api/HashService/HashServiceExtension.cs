ff0cee9485128ecb4f90609592cda503eecc8129
MichaelDiers <md@sreid.de>
2023-11-17 00:53:06 +0100

    add hash service

---
+++ WebApi.NetCore/Generic.Base.Api/HashService/HashServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace Generic.Base.Api.HashService
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    ///     Add the hash service.
+    /// </summary>
+    public static class HashServiceExtension
+    {
+        /// <summary>
+        ///     Adds the hash service.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <returns>The given <paramref name="services" />.</returns>
+        public static IServiceCollection AddHash(this IServiceCollection services)
+        {
+            return services.AddScoped<IHashService, HashService>();
+        }
+    }
+}
