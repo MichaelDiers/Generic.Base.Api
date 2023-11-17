30d1cbd463742791aa5aa74fc4250aa53351fc54
MichaelDiers <md@sreid.de>
2023-11-17 01:26:47 +0100

    add docs

--- WebApi.NetCore/Generic.Base.Api/AuthService/UserService/UserTransformer.cs
+++ WebApi.NetCore/Generic.Base.Api/AuthService/UserService/UserTransformer.cs
@@ -3,26 +3,29 @@
     using Generic.Base.Api.Result;
     using Generic.Base.Api.Transformer;
 
+    /// <summary>
+    ///     Transformer of users.
+    /// </summary>
     internal class UserTransformer : IControllerTransformer<User, ResultUser>, IAtomicTransformer<User, User, User>
     {
         /// <summary>
-        ///     Transforms the specified <paramref name="createEntry" /> of type <typeparamref name="TCreateEntry" /> to
-        ///     <typeparamref name="TEntry" />.
+        ///     Transforms the specified <paramref name="user" /> of type <see cref="User" /> to
+        ///     <see cref="User" />.
         /// </summary>
-        /// <param name="createEntry">The data for creating an entry of type <typeparamref name="TEntry" />.</param>
-        /// <returns>The transformed entry of type <typeparamref name="TEntry" />.</returns>
+        /// <param name="user">The data for creating an entry of type <see cref="User" />.</param>
+        /// <returns>The transformed entry of type <see cref="User" />.</returns>
         public User Transform(User user)
         {
             return user;
         }
 
         /// <summary>
-        ///     Transforms the specified <paramref name="updateEntry" /> of type <typeparamref name="TUpdateEntry" /> to
-        ///     <typeparamref name="TEntry" />.
+        ///     Transforms the specified <paramref name="user" /> of type <see cref="User" /> to
+        ///     <see cref="User" />.
         /// </summary>
-        /// <param name="updateEntry">The data for creating an entry of type <typeparamref name="TEntry" />.</param>
+        /// <param name="user">The data for creating an entry of type <see cref="User" />.</param>
         /// <param name="id">The id of the entry to be updated.</param>
-        /// <returns>The transformed entry of type <typeparamref name="TEntry" />.</returns>
+        /// <returns>The transformed entry of type <see cref="User" />.</returns>
         public User Transform(User user, string id)
         {
             return user;
@@ -31,7 +34,7 @@
         /// <summary>
         ///     Transforms the specified entry of <see cref="User" /> to <see cref="ResultUser" />.
         /// </summary>
-        /// <param name="entry">The data for creating an instance of <see cref="ResultUser" />.</param>
+        /// <param name="user">The data for creating an instance of <see cref="ResultUser" />.</param>
         /// <param name="links">The urn that describe the supported operations on the result entry.</param>
         /// <returns>The transformed entry of type <see cref="User" />.</returns>
         public ResultUser Transform(User user, IEnumerable<ILink> links)
