8db1a90be6a48d8ee1be97442665ec7d23bcf24a
MichaelDiers <md@sreid.de>
2023-11-18 16:17:59 +0100

    add tests for configuration extensions

---
+++ WebApi.NetCore/Generic.Base.Api.Tests/Extensions/ConfigurationExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Generic.Base.Api.Tests.Extensions
+{
+    using Generic.Base.Api.Extensions;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+
+    /// <summary>
+    ///     Tests for <see cref="Api.Extensions.ConfigurationExtensions" />.
+    /// </summary>
+    public class ConfigurationExtensionsTests
+    {
+        [Fact]
+        public void ReadFromConfigurationShouldFailIfSectionDoesNotExist()
+        {
+            var builder = new HostApplicationBuilder();
+
+            Assert.Throws<ArgumentException>(
+                () => builder.Configuration.ReadFromConfiguration<ExtensionTest>(nameof(ExtensionTest)));
+        }
+
+        [Fact]
+        public void ReadFromConfigurationShouldSucceedIfSectionExists()
+        {
+            var builder = new HostApplicationBuilder();
+            builder.Configuration.AddJsonFile("Extensions/test.json");
+
+            var actual = builder.Configuration.ReadFromConfiguration<ExtensionTest>(nameof(ExtensionTest));
+            Assert.NotNull(actual);
+
+            Assert.Equal(
+                $"{nameof(ExtensionTest)}Value1",
+                actual.ExtensionTestKey1);
+            Assert.Equal(
+                $"{nameof(ExtensionTest)}Value2",
+                actual.ExtensionTestKey2);
+        }
+    }
+}
