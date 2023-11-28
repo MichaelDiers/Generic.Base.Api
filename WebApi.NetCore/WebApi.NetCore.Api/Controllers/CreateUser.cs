5263383f4925cb0b35b8fc3b0dd4ab97b59f2714
MichaelDiers <md@sreid.de>
2023-11-28 19:12:36 +0100

    clean up

--- WebApi.NetCore/WebApi.NetCore.Api/Controllers/CreateUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApi.NetCore.Api.Controllers
-{
-    public class CreateUser
-    {
-        public string Id { get; set; }
-        public string Password { get; set; }
-    }
-}
--- WebApi.NetCore/WebApi.NetCore.Api/Items/ItemDatabaseConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApi.NetCore.Api.Items
-{
-    using WebApi.NetCore.Api.Contracts.Configuration;
-    using WebApi.NetCore.Api.Models.Configuration;
-
-    public class ItemDatabaseConfiguration : DatabaseConfiguration, IItemDatabaseConfiguration
-    {
-    }
-}
