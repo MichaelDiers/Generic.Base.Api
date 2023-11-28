6da828743e74a55e4678d861a839d94e30a03c33
MichaelDiers <md@sreid.de>
2023-11-28 21:39:00 +0100

    add test

---
+++ Generic.Base.Api.Test.Lib/Generic.Base.Api.Test.Lib.Tests/Extensions/HttpClientExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Generic.Base.Api.Test.Lib.Tests.Extensions
+{
+    using Generic.Base.Api.Tests.Lib.Extensions;
+
+    /// <summary>
+    ///     Tests for <see cref="HttpClientExtensions" />.
+    /// </summary>
+    public class HttpClientExtensionsTests
+    {
+        [Fact]
+        public void AddApiKey()
+        {
+            const string apiKey = nameof(apiKey);
+
+            using var client = new HttpClient();
+            client.AddApiKey(apiKey);
+
+            Assert.Equal(
+                apiKey,
+                client.DefaultRequestHeaders.GetValues("x-api-key").FirstOrDefault());
+        }
+    }
+}
