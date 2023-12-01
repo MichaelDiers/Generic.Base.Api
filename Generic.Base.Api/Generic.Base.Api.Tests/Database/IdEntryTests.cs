f3f45985e95d0b3b4bbf51f5d2e3c67ef28cebcb
MichaelDiers <md@sreid.de>
2023-12-01 03:34:21 +0100

    add tests, test workflow

---
+++ Generic.Base.Api/Generic.Base.Api.Tests/Database/IdEntryTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Generic.Base.Api.Tests.Database
+{
+    using Generic.Base.Api.Database;
+
+    public class IdEntryTests
+    {
+        [Fact]
+        public void Ctor()
+        {
+            var idEntry = new IdEntry(nameof(IdEntry));
+
+            Assert.Equal(
+                nameof(idEntry),
+                idEntry.Id);
+        }
+    }
+}
